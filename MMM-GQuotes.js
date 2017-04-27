96f66b394afe308a29fa71707dc8ee3c71625835
John Wade <cowboysdude@stny.rr.com>
2017-04-27 17:13:58 -0400

    Add files via upload

---
+++ MMM-GQuotes.js
@@ -0,0 +1,124 @@
+ /* Magic Mirror
+    * Module: MMM-MMM-GQuotes
+    *
+    * By cowboysdude
+    * 
+    */
+   
+Module.register("MMM-GQuotes", {
+
+       // Module config defaults.
+       defaults: {
+           updateInterval: 12*60*60*1000, // every 12 hours
+           animationSpeed: 1000,
+           initialLoadDelay: 1130, // 0 seconds delay
+           retryDelay: 2500,
+           header: "",
+           maxWidth: "100%",
+       },
+
+       // Define required scripts.
+       getScripts: function() {
+           return ["moment.js"];
+       },
+       
+       getStyles: function() {
+           return ["MMM-GQuotes.css"];
+       },
+
+       // Define start sequence.
+       start: function() {
+           Log.info("Starting module: " + this.name);
+
+           // Set locale.
+           moment.locale(config.language);
+           this.today = "";
+           //this.gquote = [];
+           this.scheduleUpdate();
+       },
+
+      getDom: function() {
+
+         var gquote = this.gquote;
+console.log(gquote);
+         for (var i = 0; i < gquote.length; i++) {
+            
+         var gquote = gquote[i];
+
+
+         var wrapper = document.createElement("div");
+         wrapper.className = "wrapper";
+         wrapper.style.maxWidth = this.config.maxWidth;
+         
+
+         if (!this.loaded) {
+             wrapper.innerHTML = "Finding Quote....";
+             wrapper.className = "bright light small";
+             return wrapper;
+         }
+         if (this.config.header != "" ){
+         var header = document.createElement("header");
+         header.className = "header";
+         header.innerHTML = this.config.header;
+         wrapper.appendChild(header);
+		 }
+		 
+         var top = document.createElement("div");
+
+         var mainquote = document.createElement("h3");
+         mainquote.classList.add("small", "bright", "content");
+         mainquote.innerHTML = gquote.description[0];
+         top.appendChild(mainquote);
+
+         var author = document.createElement("p");
+         author.classList.add("xsmall", "bright");
+         author.innerHTML = "~ "+gquote.author;
+         top.appendChild(author);
+		}
+         wrapper.appendChild(top);
+         return wrapper;
+
+     },
+
+     processGQuote: function(data) {
+         this.today = data.Today;
+         this.gquote = data;
+    console.log(this.gquote);
+         this.loaded = true;
+     },
+
+     scheduleUpdate: function() {
+         setInterval(() => {
+             this.getGQuote();
+         }, this.config.updateInterval);
+
+         this.getGQuote(this.config.initialLoadDelay);
+     },
+
+       deUmlaut: function (value){
+       value = value.replace(/ä/g, 'ae');
+       value = value.replace(/ö/g, 'oe');
+       value = value.replace(/ü/g, 'ue');
+       value = value.replace(/ß/g, 'ss');
+       value = value.replace(/ /g, '-');
+       value = value.replace(/\./g, '');
+       value = value.replace(/,/g, '');
+       value = value.replace(/\(/g, '');
+       value = value.replace(/\)/g, '');
+       return value;
+      }, 
+
+
+     getGQuote: function() {
+         this.sendSocketNotification('GET_GQUOTE');
+     },
+
+     socketNotificationReceived: function(notification, payload) {
+         if (notification === "GQUOTE_RESULT") {
+             this.processGQuote(payload);
+             this.updateDom(this.config.fadeSpeed);
+         }
+         this.updateDom(this.config.initialLoadDelay);
+     },
+
+ });
---
+++ node_helper.js
@@ -0,0 +1,75 @@
+/* Magic Mirror
+    * Module: MMM-GQuotes
+    *
+    * By Cowboysdude
+    * 
+    */
+const NodeHelper = require('node_helper');
+const request = require('request');
+const fs = require('fs');
+const parser = require('xml2js').parseString;
+var stripNS = require('xml2js').processors.stripPrefix;
+var iconv = require('iconv-lite');
+
+module.exports = NodeHelper.create({
+
+    start: function() {
+        this.gquote = {
+            timestamp: null,
+            data: null
+        };
+        this.path = "modules/MMM-GQuotes/quote.json";
+        if (fs.existsSync(this.path)) {
+            var temp = JSON.parse(fs.readFileSync(this.path));
+            if (temp.timestamp === this.getDate()) {
+                this.gquote = temp;
+            }
+        }
+    },
+
+    getGQuote: function(url) {
+    	request({ 
+    	          url:"http://www.zitate-online.de/neuestezitate.xml",
+    	          method: 'GET',
+    	          setEncoding: ('ISO-8859-1', 'UTF8')
+    	        }, (error, response, body) => {
+            if (!error && response.statusCode === 200) {
+    console.log(body);
+            var utf8String = iconv.decode(new Buffer(body), "ISO-8859-1");	
+                parser(utf8String, { tagNameProcessors: [stripNS] },  (err, result)=> {
+                        var result = JSON.parse(JSON.stringify(result.RDF.item));
+                        this.sendSocketNotification('GQUOTE_RESULT', result);
+                        this.gquote.timestamp = this.getDate();
+                        this.gquote.data = result;
+                           this.fileWrite();
+                
+                });
+            }
+       });
+    },
+    
+    fileWrite: function() {
+        fs.writeFile(this.path, JSON.stringify(this.gquote), function(err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("The Quote was saved!");
+        });
+    },
+
+    getDate: function() {
+        return (new Date()).toLocaleDateString();
+    },
+
+    //Subclass socketNotificationReceived received.
+    socketNotificationReceived: function(notification, payload) {
+        if (notification === 'GET_GQUOTE') {
+            if (this.gquote.timestamp === this.getDate() && this.gquote.data !== null) {
+                this.sendSocketNotification('GQUOTE_RESULT', this.gquote.data);
+            } else {
+                this.getGQuote(payload);
+            }
+        }
+    }
+
+});
